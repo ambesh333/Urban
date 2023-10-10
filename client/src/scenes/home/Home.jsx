a6a8f40d873dcca0ca6fd8726aa8e2dcf4801a9c
Ambesh <ambeshgaunker@gmail.com>
2023-10-10 20:35:25 +0530

    first commit

---
+++ client/src/scenes/home/Home.jsx
@@ -0,0 +1,12 @@
+import MainCarousel from "./MainCarousel.jsx";
+import ShoppingList from "./ShoppingList.jsx";
+import Subscribe from "./Subscribe.jsx";
+const Home = () => {
+    return <div className="home">
+        <MainCarousel/>
+        <ShoppingList/>
+        <Subscribe/>
+    </div>;
+}
+
+export default Home;
